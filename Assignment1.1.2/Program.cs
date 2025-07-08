d62f012d8c7610606d5fc4fa5322c723bdde1bd8
15127 <15127@DESKTOP-0V74CCT>
2025-07-08 10:33:38 -0500

    Add project files.

---
+++ Assignment1.1.2/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Assignment1._1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("You will be adding two numbers input your first number");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input your second number");
+            int y = Convert.ToInt32(Console.ReadLine());
+            int finalNumber = x + y;
+            Console.WriteLine(finalNumber);
+
+        }
+    }
+}
